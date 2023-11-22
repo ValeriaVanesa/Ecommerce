0341a8643b741f82f797eb88d83df1ae5bff794a
Gerardo Daniel Klos <gklos002@gmail.com>
2023-11-21 23:05:14 -0300

    cambios gerardo

--- public/js/productos.js
+++ public/js/productos.js
@@ -12,105 +12,105 @@ const verCarrito=document.getElementById("verCarrito");
 const modalContainer=document.getElementById("modal-container");
 
 
-const productos =[
+// const productos =[
     
-   {
-   id: 1,
-   nombre:"Zapatilla Vans",
-   precio:13900,
-   img: "https://i.pinimg.com/originals/92/84/91/928491a8a6f4b7a09af0f5a205ada2c4.jpg ",
-   cantidad:1,
-},
-   {
-       id: 2,
-       nombre:"Bota Vans",
-       precio:14900,
-       img:
-       "  https://i.pinimg.com/originals/2f/69/6b/2f696bd463405865abfc2313bac7afc0.jpg ",
-       cantidad:1,
+//    {
+//    id: 1,
+//    nombre:"Zapatilla Vans",
+//    precio:13900,
+//    img: "https://i.pinimg.com/originals/92/84/91/928491a8a6f4b7a09af0f5a205ada2c4.jpg ",
+//    cantidad:1,
+// },
+//    {
+//        id: 2,
+//        nombre:"Bota Vans",
+//        precio:14900,
+//        img:
+//        "  https://i.pinimg.com/originals/2f/69/6b/2f696bd463405865abfc2313bac7afc0.jpg ",
+//        cantidad:1,
        
-    },
-    {
-       id: 3,
-       nombre:"Zapatilla Vans",
-       precio:11900,
-       img:
-       "https://http2.mlstatic.com/D_Q_NP_788540-MLB27433382299_052018-Q.jpg ",
+//     },
+//     {
+//        id: 3,
+//        nombre:"Zapatilla Vans",
+//        precio:11900,
+//        img:
+//        "https://http2.mlstatic.com/D_Q_NP_788540-MLB27433382299_052018-Q.jpg ",
        
-       cantidad:1,
-    },    
-    {
-       id: 4,
-       nombre:"Zapatilla Vans",
-       precio:15000,
-       img:
+//        cantidad:1,
+//     },    
+//     {
+//        id: 4,
+//        nombre:"Zapatilla Vans",
+//        precio:15000,
+//        img:
        
-       " https://i.pinimg.com/736x/d8/6b/2d/d86b2d4773b89e6dcb7c8a4145a1962f--vans-footwear-vans-sneakers.jpg",
-       cantidad:1,
+//        " https://i.pinimg.com/736x/d8/6b/2d/d86b2d4773b89e6dcb7c8a4145a1962f--vans-footwear-vans-sneakers.jpg",
+//        cantidad:1,
        
-    } ,
-    {
-       id: 5,
-       nombre:"Zapatilla Converse",
-       precio:13900,
-       img:
-       " https://th.bing.com/th/id/OIP.bQdo4Q1syg5cG-QwyfCUlgHaHa?pid=ImgDet&w=500&h=500&rs=1 ",
-       cantidad:1,
-      } ,  
+//     } ,
+//     {
+//        id: 5,
+//        nombre:"Zapatilla Converse",
+//        precio:13900,
+//        img:
+//        " https://th.bing.com/th/id/OIP.bQdo4Q1syg5cG-QwyfCUlgHaHa?pid=ImgDet&w=500&h=500&rs=1 ",
+//        cantidad:1,
+//       } ,  
   
 
-   {
-      id: 12,
-      nombre:"Tease Victorias Secret",
-      precio:14900,
-      img: 
-      " https://th.bing.com/th/id/OIP.Cn7vAAGO9_m--a0JpfxCQwHaJ3?pid=ImgDet&w=760&h=1013&rs=1 ",
-      cantidad:1,
-   },   
+//    {
+//       id: 12,
+//       nombre:"Tease Victorias Secret",
+//       precio:14900,
+//       img: 
+//       " https://th.bing.com/th/id/OIP.Cn7vAAGO9_m--a0JpfxCQwHaJ3?pid=ImgDet&w=760&h=1013&rs=1 ",
+//       cantidad:1,
+//    },   
 
-   {
-      id: 13,
-      nombre:"La vie est belle Lancome ",
-      precio:14900,
-      img: 
-      "https://i.pinimg.com/originals/4a/4b/f9/4a4bf9d5a1653d48d8deebae10bc7e9b.jpg ",
-      cantidad:1,
-   },   
-   {
-      id: 14,
-      nombre:"Mon Paris Yves Saint Laurent ",
-      precio:14900,
-      img: 
-      " https://th.bing.com/th/id/OIP.dsPuVmzkzudpGNVqYkkMsQHaJ4?pid=ImgDet&w=1440&h=1920&rs=1 ",
-      cantidad:1,
-   },   
-   {
-      id: 15,
-      nombre:" Good Girl Blush",
-      precio:15000,
-      img: 
-      "https://th.bing.com/th/id/OIP.ZYfKdznaWKVnlULsW2qcagHaHa?pid=ImgDet&rs=1 ",
-      cantidad:1,
-   },   
-   {
-      id: 16,
-      nombre:"Bombshell Victorias Secret ",
-      precio:15000,
-      img: 
-      " https://th.bing.com/th/id/R.58d1bdaba417057ab6fcb136f3e72013?rik=UhrZuu38WB7IaA&pid=ImgRaw&r=0 ",
-      cantidad:1,
-   },   
-   {
-      id: 17,
-      nombre:" Perfect Marc Jacobs",
-      precio:15000,
-      img: 
-      "  https://www.clara.es/medio/2020/11/27/marc-jacobs-perfect-eau-de-parfum-para-mujer_c4104fa5_500x750.jpg",
-      cantidad:1,
-   },   
+//    {
+//       id: 13,
+//       nombre:"La vie est belle Lancome ",
+//       precio:14900,
+//       img: 
+//       "https://i.pinimg.com/originals/4a/4b/f9/4a4bf9d5a1653d48d8deebae10bc7e9b.jpg ",
+//       cantidad:1,
+//    },   
+//    {
+//       id: 14,
+//       nombre:"Mon Paris Yves Saint Laurent ",
+//       precio:14900,
+//       img: 
+//       " https://th.bing.com/th/id/OIP.dsPuVmzkzudpGNVqYkkMsQHaJ4?pid=ImgDet&w=1440&h=1920&rs=1 ",
+//       cantidad:1,
+//    },   
+//    {
+//       id: 15,
+//       nombre:" Good Girl Blush",
+//       precio:15000,
+//       img: 
+//       "https://th.bing.com/th/id/OIP.ZYfKdznaWKVnlULsW2qcagHaHa?pid=ImgDet&rs=1 ",
+//       cantidad:1,
+//    },   
+//    {
+//       id: 16,
+//       nombre:"Bombshell Victorias Secret ",
+//       precio:15000,
+//       img: 
+//       " https://th.bing.com/th/id/R.58d1bdaba417057ab6fcb136f3e72013?rik=UhrZuu38WB7IaA&pid=ImgRaw&r=0 ",
+//       cantidad:1,
+//    },   
+//    {
+//       id: 17,
+//       nombre:" Perfect Marc Jacobs",
+//       precio:15000,
+//       img: 
+//       "  https://www.clara.es/medio/2020/11/27/marc-jacobs-perfect-eau-de-parfum-para-mujer_c4104fa5_500x750.jpg",
+//       cantidad:1,
+//    },   
 
    
-   ];
+//    ];
 
 
 let carrito=[];
